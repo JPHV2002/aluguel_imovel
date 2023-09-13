cf4263b66a61cb8a52c6ecbc029c0d67091ece2c
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 18:06:14 -0300

    feat: config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { User } from "./models/User";
+import { dbPassword, dbUser } from "./environment";
+import { authRouter } from "./routes/auth";
+
+
 
 const app = express();
 
-app.get("/", (req, res) => {
-    console.log(" Teste");
-    res.status(200).send("Teste");
-});
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors())
+app.use(cookieParser());
 
-app.listen(8080, () => {
-    console.log("App is on 8080")
+app.use("/auth", authRouter)
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello")
 })
+
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.vlx9dpn.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    app.listen(8080)
+    console.log("Conectou ao banco")
+}).catch((err) => console.log(err))
+
