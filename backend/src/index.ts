6861a0ed68b9910bba13f51fbba0b8862bf1b550
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 19:35:34 -0300

    feat: properties routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import { User } from "./models/User";
 import { dbPassword, dbUser } from "./environment";
 import { authRouter } from "./routes/auth";
+import { propertieRouter } from "./routes/properties";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use("/auth", authRouter);
+app.use("/properties", propertieRouter)
 
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.vlx9dpn.mongodb.net/?retryWrites=true&w=majority`).then(() => {
