7797b0d50653f7c1703a7375121f5be8f60b2c0e
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 19:34:59 -0300

    feat: properties joi

--- backend/src/models/Properties.ts
+++ backend/src/models/Properties.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const propertiesSchema = new mongoose.Schema({
+    ownerId: String,
     name: String,
-    ownerEmail: String,
-    
+    numRoom: Number,
+    numBathroom: Number,
+    city: String,
 });
 
 export const Properties = mongoose.model("Properties", propertiesSchema);
