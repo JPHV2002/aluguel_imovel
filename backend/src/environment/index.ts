5f9a0778909077ad2355f2bf5f3b47fca3952659
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 18:40:10 -0300

    feat: secret

--- backend/src/environment/index.ts
+++ backend/src/environment/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 
 dotenv.config()
-export const dbUser = process.env.DB_USER || "";
-export const dbPassword = process.env.DB_PASSWORD || "";
+export const dbUser = process.env.DB_USER || "jpvellenich";
+export const dbPassword = process.env.DB_PASSWORD || "f6NIvSFSdF4FUy0K";
+export const secret = process.env.SECRET || "asdkvonoeiivnaoienalsdkj3234dnvaoisd"
