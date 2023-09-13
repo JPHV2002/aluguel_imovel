b39c17d030b0423f52ecdc9a87c835dae89e7497
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 18:40:24 -0300

    feat: login route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { User } from "../models/User";
-import { createUser } from "../controllers/auth";
+import { createUser, loginUser } from "../controllers/auth";
 
 export const authRouter = Router()
 
 authRouter.post("/register", createUser);
+authRouter.post("/login", loginUser)
