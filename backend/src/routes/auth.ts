6f77dfe6d08d4a06d9f6c5c74fe8446457eb6d76
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 18:23:38 -0300

    feat: create user route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,20 +1,7 @@
 import { Router } from "express";
 import { User } from "../models/User";
+import { createUser } from "../controllers/auth";
 
 export const authRouter = Router()
 
-authRouter.post("/register", async (req, res) => {
-    const {name, email, password, confirmPassword} = req.body;
-    const user = new User({
-        name: name,
-        email: email,
-        password: password
-    })
-    try {
-        user.save()
-        res.status(200).send("all done")
-    } catch (error) {
-        console.log(error)
-        res.status(400).send(error)
-    }
-})
+authRouter.post("/register", createUser);
