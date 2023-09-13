5d784316c8e9a78600e6fc8b74dce23e894f0a2f
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 18:48:49 -0300

    feat: private route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { User } from "../models/User";
 import { createUser, loginUser } from "../controllers/auth";
+import { authMiddleware } from "../middlewares/auth";
 
-export const authRouter = Router()
+export const authRouter = Router();
 
 authRouter.post("/register", createUser);
-authRouter.post("/login", loginUser)
+authRouter.post("/login", loginUser);
+
+authRouter.get("/", authMiddleware, (req, res) => {
+    console.log(" a");
+    res.status(200).send(" a")
+})
