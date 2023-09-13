db5ba34bca5108d9cab43c56f90daaa91aab6910
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 18:41:02 -0300

    feat: add router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,30 +2,23 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+
 import { User } from "./models/User";
 import { dbPassword, dbUser } from "./environment";
 import { authRouter } from "./routes/auth";
-
-
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors())
+app.use(cors());
 app.use(cookieParser());
 
-app.use("/auth", authRouter)
-
-app.get("/", (req, res) => {
-    res.status(200).send("Hello")
-})
+app.use("/auth", authRouter);
 
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.vlx9dpn.mongodb.net/?retryWrites=true&w=majority`).then(() => {
     app.listen(8080)
     console.log("Conectou ao banco")
-}).catch((err) => console.log(err))
+}).catch((err) => console.log(err));
 
