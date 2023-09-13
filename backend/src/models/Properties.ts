1b35d80bb52674caab929844fe3ff103644e3370
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 18:49:28 -0300

    feat: poperties model

--- backend/src/models/Properties.ts
+++ backend/src/models/Properties.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 const propertiesSchema = new mongoose.Schema({
-
+    name: String,
+    ownerEmail: String,
+    
 });
 
 export const Properties = mongoose.model("Properties", propertiesSchema);
