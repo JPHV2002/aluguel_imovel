9a9abbd08e8f348b4b0350b09c869c86110e27b1
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 18:23:03 -0300

    feat: user joi

---
+++ backend/src/utils/joi/userJoi.ts
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+export const createUserSchema = Joi.object({
+    name: Joi.string()
+        .required()
+        .min(3)
+        .max(30)
+        .trim(true)
+        .error(new Error("Erro no nome")),
+    email: Joi.string()
+        .required()
+        .min(3)
+        .max(30)
+        .trim(true)
+        .error(new Error("Erro no email")),
+    password: Joi.string()
+        .required()
+        .min(3)
+        .max(30)
+        .trim(true)
+        .error(new Error("Erro na senha")),
+    confirmPassword: Joi.string()
+        .required()
+        .min(3)
+        .max(30)
+        .trim(true)
+        .error(new Error("Erro na confirmacao de senha")),
+})
