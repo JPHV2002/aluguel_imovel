523ceb300493866b5487e294aa226e3522c3ffa8
JPHV2002 <jpvellenich@gmail.com>
2023-09-13 19:35:59 -0300

    fix: fix auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,13 +1,7 @@
 import { Router } from "express";
 import { createUser, loginUser } from "../controllers/auth";
-import { authMiddleware } from "../middlewares/auth";
 
 export const authRouter = Router();
 
 authRouter.post("/register", createUser);
 authRouter.post("/login", loginUser);
-
-authRouter.get("/", authMiddleware, (req, res) => {
-    console.log(" a");
-    res.status(200).send(" a")
-})
